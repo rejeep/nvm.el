1f171194ae338a4ae0552b994b06a3c617830876
Johan Andersson <johan.rejeep@gmail.com>
2013-12-18 15:05:24 +0100

    Add test setup.

---
+++ test/test-helper.el
@@ -0,0 +1,10 @@
+(require 'f)
+
+(defvar nvm-test/test-path
+  (f-parent (f-this-file)))
+
+(defvar nvm-test/root-path
+  (f-parent nvm-test/test-path))
+
+(require 'ert)
+(require 'nvm (f-expand "nvm" nvm-test/root-path))

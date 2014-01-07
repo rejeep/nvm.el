57f4e8e9b70367ccf3f08d64e7b244981c8345c6
Johan Andersson <johan.rejeep@gmail.com>
2014-01-07 09:38:56 +0100

    Release v0.0.2.

--- nvm.el
+++ nvm.el
@@ -4,7 +4,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Keywords: node, nvm
 ;; URL: http://github.com/rejeep/nvm.el
 ;; Package-Requires: ((s "1.8.0") (dash "2.4.0") (f "0.14.0"))
